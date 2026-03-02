2f3bdba8ba542ecd97dc99c95039b927aaed7b12
JoseAB32 <arias.balderrama.josmearia@gmail.com>
2026-03-02 00:12:42 -0400

    first commit: Creacion repositorio

---
+++ travel-backend/src/test/java/com/traveling/travel_backend/TravelBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.traveling.travel_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
