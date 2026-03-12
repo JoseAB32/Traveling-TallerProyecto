4b6e42cbfc5508b9e1c46cb71e5235875248f398
Vanessa745 <vanessa.canaviri@ucb.edu.bo>
2026-03-11 23:28:06 -0400

    fix: corrección de application.properties URL de conexión a la BD

--- travel-backend/src/main/java/com/traveling/travel_backend/controller/userController.java
+++ travel-backend/src/main/java/com/traveling/travel_backend/controller/userController.java
@@ -1,16 +1,28 @@
 package com.traveling.travel_backend.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.traveling.travel_backend.model.User;
 import com.traveling.travel_backend.repository.UserRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @RestController
 @RequestMapping("api/")
 @CrossOrigin(origins = "http://localhost:4200")
 public class userController {
+
     @Autowired
     private UserRepository userRepository;
+
+    @GetMapping("/users")
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+    
 }
