c2a5040f755301c01523526eb17d6d904abdc61f
Vanessa745 <vanessa.canaviri@ucb.edu.bo>
2026-03-12 12:24:31 -0400

    feat: creación de usuarios

--- travel-frontend/src/app/app.routes.ts
+++ travel-frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { InicioComponent } from './inicio/inicio.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'sign-up', component: SignUpComponent },
+    { path: 'inicio', component: InicioComponent }
+];
--- travel-frontend/src/app/sign-up/sign-up.component.ts
+++ travel-frontend/src/app/sign-up/sign-up.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from '../user';
+import { FormsModule } from '@angular/forms';
+import { UserService } from '../user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
-export class SignUpComponent {
+export class SignUpComponent implements OnInit {
+  user: User = new User();
 
+  constructor (private userService: UserService, private router: Router) {
+
+  }
+
+  ngOnInit(): void {
+      
+  }
+
+  saveUser() {
+    this.userService.createUser(this.user).subscribe( data => {
+        console.log(data);
+        this.goToInicio();
+      },
+      error => console.log(error)
+    );
+  }
+
+  goToInicio() {
+    this.router.navigate(['/inicio']);
+  }
+
+  onSubmit() {
+    console.log(this.user);
+    this.saveUser();
+  }
 }
