5218e59f12c3a705752ebe3f924830e7651b90c7
JoseAB32 <arias.balderrama.josmearia@gmail.com>
2026-03-12 18:32:26 -0400

    feat: PaginaLogueado v1

--- travel-frontend/src/app/app.routes.ts
+++ travel-frontend/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { InicioLogueadoComponent } from './inicio-logueado/inicio-logueado.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'InicioLogueado', component: InicioLogueadoComponent }
+];
