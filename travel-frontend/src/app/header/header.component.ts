92c189f1480237625a360cea89ce75465dbe4088
Mateo Gandarillas <mateoganav@gmail.com>
2026-03-15 17:14:06 -0400

    feat: Search bar & dropdown menu V1

--- travel-frontend/src/app/header/header.component.ts
+++ travel-frontend/src/app/header/header.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  /*
+  Flags para probar que se muestre solo cuando hay usuari0 loggeado,
+  faltaría conectar con lógica actual
+  */
+  isLoggedIn: boolean = false; 
+  
+  // Controla el menú desplegable
+  isMenuOpen: boolean = true;
 
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
