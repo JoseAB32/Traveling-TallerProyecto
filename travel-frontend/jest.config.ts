5d839584c93087398c0fd80eb2b22c378ce5e387
Vanessa745 <vanessa.canaviri@ucb.edu.bo>
2026-05-05 13:06:21 -0400

    feat: instalación de jest y mejora visual de carrusel

---
+++ travel-frontend/jest.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  testEnvironment: 'jsdom',
+  testMatch: ['**/*.spec.ts'],
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    'src/app/**/*.ts',
+    '!src/app/**/*.module.ts',
+    '!src/app/**/*.routes.ts',
+    '!src/main.ts',
+    '!src/environments/**'
+  ]
+};
+
+export default config;
