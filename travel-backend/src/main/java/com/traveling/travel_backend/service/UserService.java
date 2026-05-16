f57ee6a9355e7dd6e74e7f6e6fc379f2a90f6651
Mateo Gandarillas <mateoganav@gmail.com>
2026-05-15 20:01:38 -0400

    test(User): New test for profile on UserServiceTest.java

--- travel-backend/src/main/java/com/traveling/travel_backend/service/UserService.java
+++ travel-backend/src/main/java/com/traveling/travel_backend/service/UserService.java
@@ -21,8 +21,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.security.core.Authentication;
 
-import org.springframework.security.core.Authentication;
-
 import java.util.List;
 import java.util.stream.Collectors;
 
