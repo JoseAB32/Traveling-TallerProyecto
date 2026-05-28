09148bae023f097a6d578e612d2eccdb19f5ac89
Vanessa745 <vanessa.canaviri@ucb.edu.bo>
2026-05-27 22:34:05 -0400

    feat: control de token expirado

---
+++ travel-frontend/src/app/interceptors/authError.interceptor.ts
@@ -0,0 +1,30 @@
+import { HttpInterceptorFn, HttpErrorResponse } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError, throwError } from 'rxjs';
+
+export const authErrorInterceptor: HttpInterceptorFn = (req, next) => {
+  const router = inject(Router);
+
+  return next(req).pipe(
+    catchError((error: HttpErrorResponse) => {
+    //   console.log('Error interceptado:', {
+    //     status: error.status,
+    //     message: error.message,
+    //     url: error.url,
+    //     error: error.error
+    //   });
+  
+      if (error.status === 401 || error.status === 403) {
+        localStorage.removeItem('token');
+        localStorage.removeItem('user');
+
+        router.navigate(['/login'], {
+          queryParams: { sessionExpired: 'true' }
+        });
+      }
+
+      return throwError(() => error);
+    })
+  );
+};
