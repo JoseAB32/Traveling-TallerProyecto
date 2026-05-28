0ce70b74e3559a253b824b2a29a816b7e8607575
Marcelo <marcelo.navia@ucb.edu.bo>
2026-05-28 16:08:24 -0400

    Correcion de TravelBackendApplication

--- travel-backend/src/main/java/com/traveling/travel_backend/TravelBackendApplication.java
+++ travel-backend/src/main/java/com/traveling/travel_backend/TravelBackendApplication.java
@@ -2,12 +2,17 @@ package com.traveling.travel_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import com.traveling.travel_backend.TravelBackendApplication;
 
 @SpringBootApplication
+@EnableScheduling
+@PropertySource("classpath:application.properties")
 public class TravelBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TravelBackendApplication.class, args);	
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(TravelBackendApplication.class, args);
+    }
 }
