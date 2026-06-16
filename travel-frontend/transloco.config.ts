9020c0563d56ee66b90a88057198a72a84da9b36
Vanessa745 <vanessa.canaviri@ucb.edu.bo>
2026-06-16 17:04:17 -0400

    Se añadieron los archivos para manejar los idiomas Frances, Portugues e Italiano

--- travel-frontend/transloco.config.ts
+++ travel-frontend/transloco.config.ts
@@ -2,7 +2,7 @@ import {TranslocoGlobalConfig} from '@jsverse/transloco-utils';
     
 const config: TranslocoGlobalConfig = {
   rootTranslationsPath: 'public/i18n/',
-  langs: [ 'es', 'en' ],
+  langs: [ 'es', 'en', 'fr', 'pt', 'it' ],
   keysManager: {}
 };
     
